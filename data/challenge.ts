45d2eb92068153121a4a6abee4317470947bd65f
Monarch Wadia <monarch@mintbean.io>
2021-12-25 17:02:34 -0500

    Moved static assets to static route

--- data/challenge.ts
+++ data/challenge.ts
@@ -1,3 +1,6 @@
 import { getPrisma } from "../getPrisma";
 
 export const getChallenges = async () => getPrisma().challenge.findMany({})
+export const getChallengeBy = async ({id}: {id: string}) => getPrisma().challenge.findUnique({where: {
+  id
+}})
--- index.ts
+++ index.ts
@@ -2,25 +2,37 @@ import Koa from 'koa';
 import Pug from "koa-pug";
 import KoaRouter from "koa-router"
 import path from "path";
-import { getChallenges } from './data/challenge';
+import { getChallengeBy, getChallenges } from './data/challenge';
 import serve from "koa-static";
+import mount from 'koa-mount';
 
 const app = new Koa();
-app.use(serve(path.resolve(__dirname, "./static")))
 
+// static assets
+app.use(mount("/static", serve(path.resolve(__dirname, "./static"))));
+
+// Pug templating engine
 // this is weird, but it's how Pug initializes stuff.
 new Pug({
   viewPath: path.resolve(__dirname, "./views"),
   app
 })
+
+// main router
 const router = new KoaRouter();
 
+// static routes
 ["about", "discord", "employers", "guide", "layout", "about", "login"].forEach(route => {
   router.get(`/${route}`, async (ctx) => await ctx.render(route))
 });
 
+// home page
 router.get("/", async (ctx) => await ctx.render("index", {challenges: await getChallenges()}))
 
+// challenge routes
+router.get("/challenge/:id", async (ctx) => await ctx.render("challenge/view", {challenge: await getChallengeBy({id: ctx.params.id})}))
+
+// register the routes
 app.use(router.routes());
 
 app.listen(3000);
