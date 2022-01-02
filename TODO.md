ed37730fc91fca83f186fe3fcfad8fc86740dcf4
Monarch Wadia <monarch@mintbean.io>
2022-01-01 22:39:00 -0500

    Added project and comment schemas

--- TODO.md
+++ TODO.md
@@ -1,6 +1,10 @@
-# Challenges
+# NEXT!
 - User can submit projects on challenges (plaintext)
 - User can see other projects that were submitted (plaintext)
+- Projects have comment threads
+- Challenges have comment threads
+
+# Challenges
 - Challenges textareas are in markdown
 
 # Comments
@@ -8,13 +12,27 @@
 - User can leave replies to challenge comments, unlimited depth (plaintext)
 - User can leave comments on projects (plaintext, no threading)
 - User can leave replies to project comments, unlimited depth (plaintext)
-- Users receive notifications on responses & comments
+
+# Hiring managers
+- Hiring managers are marked as managers in comments
+- Hiring manager comments are highlighted
+- Hiring manager company logo shows up in comments
 
 # Reports and bans
 - User can report a comment for abuse (2 reports = timeout)
 - Admin can review projects in timeout
 - Admin can review comments in timeout
 - Admin can shadowban users
+
+# Awards
+- Admin can award a user a star
+
+# Notifications
+- Notifications show up in notification bar
+- Comment responses (immediate)
+- Submission receipt (immediate)
+- Award receipt (immediate)
+- Broken (immediate)
 
 ## Done
 
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,26 +2,114 @@ import { createUser } from "../lib/user/dao";
 import { prismaClient } from "../prismaClient";
 
 async function seed() {
-  await createUser({
+  const user = await createUser({
     email: "user@mintbean.io",
     name: "Mintbean User",
     password: "password",
     isAdmin: false
   });
 
-  await createUser({
+  const admin = await createUser({
     email: "admin@mintbean.io",
     name: "Mintbean Admin",
     password: "password",
     isAdmin: true
   });
 
-  await prismaClient.challenge.create({
+  // MUSICAL INSTRUMENT CHALLENGE 
+
+  const musicalInstrumentChallenge = await prismaClient.challenge.create({
     data: {
       title: "Build a Musical Instrument",
       description: "JavaScript is versatile and friendly for all kinds of work. Today, we'll build a musical instrument Enjoy!",
       instructions: "These are sample instructions.",
-      tags: ["JS", "React"]
+      thread: {
+        create: {}
+      },
+    }
+  })
+
+  const musicComment1 = await prismaClient.comment.create({
+    data: {
+      threadId: musicalInstrumentChallenge.threadId,
+      body: "This musical instrument thing is nice but what's the user of it?",
+      userId: user.id
+    }
+  })
+  const musicComment2 = await prismaClient.comment.create({
+    data: {
+      threadId: musicalInstrumentChallenge.threadId,
+      body: "This comment is in violation of the Prime Directive! How dare you?!",
+      userId: admin.id,
+      parentId: musicComment1.id
+    }
+  });
+  const musicComment3 = await prismaClient.comment.create({
+    data: {
+      threadId: musicalInstrumentChallenge.threadId,
+      body: "Can I make a piano?",
+      userId: user.id
+    }
+  })
+  const musicComment4 = await prismaClient.comment.create({
+    data: {
+      threadId: musicalInstrumentChallenge.threadId,
+      body: "Yes.",
+      userId: admin.id,
+      parentId: musicComment3.id
+    }
+  });
+  const musicComment5 = await prismaClient.comment.create({
+    data: {
+      threadId: musicalInstrumentChallenge.threadId,
+      body: "You can also make a death star.",
+      userId: admin.id,
+      parentId: musicComment3.id
+    }
+  });
+
+  // MUSICAL INSTRUMENT PROJECT 1
+  const project1 = await prismaClient.project.create({
+    data: {
+      deployedUrl: "https://google.com",
+      githubUrl: "https://github.com",
+      description: "Some description about this project I just made.",
+      title: "Something",
+      user: {
+        connect: {
+          id: user.id
+        }
+      },
+      challenge: {
+        connect: {
+          id: musicalInstrumentChallenge.id
+        }
+      },
+      thread: {
+        create: {}
+      },
+    }
+  })
+  const projectComment1 = await prismaClient.comment.create({
+    data: {
+      body: "This is my project and I admit, it sucks.",
+      threadId: project1.threadId,
+      userId: user.id,
+    }
+  })
+  const projectComment2 = await prismaClient.comment.create({
+    data: {
+      body: "Nah, I like it.",
+      threadId: project1.threadId,
+      userId: admin.id,
+      parentId: projectComment1.id
+    }
+  })
+  const projectComment3 = await prismaClient.comment.create({
+    data: {
+      body: "No more submissions?",
+      threadId: project1.threadId,
+      userId: admin.id,
     }
   })
 
@@ -30,7 +118,16 @@ async function seed() {
       title: "Create a paint app",
       description: "Paint apps will help you understand click event handlers, the canvas API, and SVG elements.",
       instructions: "These are sample instructions.",
-      tags: ["JS", "React"]
+      thread: {
+        create: {
+          comments: {
+            create: {
+              body: "This seems easy enough, right?",
+              userId: user.id  
+            }
+          }
+        }
+      },
     }
   })
 }
